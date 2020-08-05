4ed32753ec9885453fd0fcf73fdb5031ac1d020b
Djangogirls_after_4mounth <lightning110299@gmail.com>
2020-08-05 19:33:58 +1000

    Window resized,eraser changed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import colorchooser
 
 class Painting():
 	def __init__(self):
+		self.eraser = False
 		self.oval = 0
 		self.brush_size = 1
 
@@ -11,9 +12,13 @@ class Painting():
 
 		self.color = "black"
 		self.bg_color = "white"
+		self.width = 1200
+		self.height = 700
+		self.screen_resolution = str(self.width)+'x'+str(self.height) + "+300+300"
 		# Создание окна
 		self.root = Tk()
-		self.root.geometry("850x500+300+300")
+		self.root.geometry(self.screen_resolution)
+		self.root.resizable(width=False, height=False)
 		self.root.title("Paint")
 
 		self.root.bind("<Motion>", self.motion)
@@ -22,7 +27,7 @@ class Painting():
 		self.interface()
 
 		# Создание Канвы 
-		self.canvas = Canvas(self.root,width=850,height=500,bg=self.bg_color)
+		self.canvas = Canvas(self.root,width=self.width,height=self.height,bg=self.bg_color)
 		self.canvas.bind("<B1-Motion>",self.draw)
 		self.canvas.bind("<ButtonRelease-1>",self.null)
 		self.canvas.pack(side=BOTTOM)
@@ -38,16 +43,20 @@ class Painting():
 		btn_erase.grid(row=0,column=0,padx=4,pady=4)
 		btn_erase.bind("<Button-1>", self.erase)
 
+		btn_draw = Button(tool_bar,text="Draw",height=2,width=5)
+		btn_draw.grid(row=0,column=1,padx=4,pady=4)
+		btn_draw.bind("<Button-1>", self.btn_draw)
+
 		btn_color = Button(tool_bar,text="Color",height=2,width=5)
-		btn_color.grid(row=0,column=1,padx=4,pady=4)
+		btn_color.grid(row=0,column=2,padx=4,pady=4)
 		btn_color.bind("<Button-1>", self.choose_color)
 
 		btn_clear = Button(tool_bar,text="Clear",height=2,width=5)
-		btn_clear.grid(row=0,column=2,padx=4,pady=4)
+		btn_clear.grid(row=0,column=3,padx=4,pady=4)
 		btn_clear.bind("<Button-1>", self.clear)
 
 		self.spin = Spinbox(tool_bar, from_=1, to=100, width=5, command=self.choose_br_size)  
-		self.spin.grid(row=0, column=3)
+		self.spin.grid(row=0, column=4)
 
 	def choose_br_size(self): # Выбор размера кисти
 		self.brush_size = int(self.spin.get())
@@ -55,12 +64,19 @@ class Painting():
 	def clear(self,event): # Очистка холста
 		self.canvas.delete(ALL)
 
+	def btn_draw(self,event): # После отмены ластика, вернется прерыдущее значение цвета 
+		self.eraser = False
+		self.color = self.backup_color
+
 	def erase(self,event): # Реализация ластика
-		self.color = "white"
+		self.eraser = True
+		self.backup_color = self.color
+		self.color = self.bg_color
 
 	def choose_color(self,event): # Выбор цвета кисти
-		c = colorchooser.askcolor()
-		self.color = c[1]
+		if self.eraser != True:
+			c = colorchooser.askcolor()
+			self.color = c[1]
 
 	def null(self,event): # Сброс координат
 		self.old_x = None
